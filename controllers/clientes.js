514bb1408f946d5d1b506293e235400b9df19033
Lorena Santana <lo.santana96@gmail.com>
2022-05-25 15:07:45 -0300

    Finalização do crud de clientes

--- controllers/clientes.js
+++ controllers/clientes.js
@@ -2,11 +2,29 @@ const Cliente = require('../models/clientes')
 
 module.exports = app => {
 
-    app.get('/clientes', (req, res) => res.send('servidor rodando, tudo ok'))
+    app.get('/clientes', (req, res) => {
+        Cliente.listarClientes(res)
+    })
+
+    app.get('/clientes/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        Cliente.listarClientePorId(id, res)
+    })
 
     app.post('/clientes', (req, res) => {        
         const clientes = req.body
-        Cliente.adiciona(clientes, res)
+        Cliente.adicionaClientes(clientes, res)
+    })
+
+    app.patch('/clientes/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+        Cliente.alteraCliente(id, valores, res)
+    })
+
+    app.delete('/clientes/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        Cliente.deletaCliente(id, res)
     })
 
 }
--- models/clientes.js
+++ models/clientes.js
@@ -2,20 +2,145 @@ const conexao = require('../controllers/Infraestrutura/conexao')
 const moment = require('moment')
 
 class Cliente {
-    adiciona(clientes, res){
+
+    adicionaClientes(clientes, res){
+        //formatações
         const data_criacao = moment().format('YYYY-MM-DD HH:mm:ss')
         const data_nascimento = moment(clientes.data_nascimento, 'DD/MM/YYYY').format('YYYY-MM-DD')
         const clientesDatado = {...clientes, data_nascimento, data_criacao}
+        const idadeCliente = this.calculaIdade(clientes)
+        
+        // condicionais boleanas
+        const nomeValido = clientes.nome.length >= 5
+        const idadeValida = idadeCliente >= 18
+      
 
-        const sql = 'INSERT INTO Cliente SET ?'
-        conexao.query(sql, clientesDatado, (erro, resultados) => {
+        const validacoes = [
+            {
+                nome: 'nome',
+                valido: nomeValido,
+                mensagem: 'O nome do cliente deve ter pelo menos cinco caracteres'
+            },
+            {
+                nome: 'idade',
+                valido: idadeValida,
+                mensagem: 'O cliente precisa ser maior do que 18 anos.'
+            }
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if(existemErros) {
+            res.status(400).json(erros)
+        } else {
+
+            const sql = 'INSERT INTO Cliente SET ?'
+
+            conexao.query(sql, clientesDatado, (erro) => {
+                if(erro) {
+                    res.status(400).json(erro)
+                    } else {
+                    res.status(200).json(clientes)
+                }
+            })
+        }
+
+     
+    }
+
+    listarClientes(res) {
+
+        const sql = 'SELECT * FROM cliente'
+
+        conexao.query(sql, (erro, resultados) => {
             if(erro) {
                 res.status(400).json(erro)
-                } else {
-                res.status(200).json(clientes)
+            } else {
+                res.status(200).json(resultados)
+            }
+        })
+    }
+
+    listarClientePorId(id, res) {
+        
+        const sql = `SELECT * FROM cliente WHERE id_cliente = ${id}`
+
+        conexao.query(sql, (erro, resultados) => {
+            
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultados)
             }
         })
 
+    }
+
+    alteraCliente(id, valores, res) {
+        const data_criacao = moment().format('YYYY-MM-DD HH:mm:ss')
+        const data_nascimento = moment(valores.data_nascimento, 'DD/MM/YYYY').format('YYYY-MM-DD')
+        const idadeCliente = this.calculaIdade(valores)
+        
+        // condicionais boleanas
+        const nomeValido = valores.nome.length >= 5
+        const idadeValida = idadeCliente >= 18
+      
+
+        const validacoes = [
+            {
+                nome: 'nome',
+                valido: nomeValido,
+                mensagem: 'O nome do cliente deve ter pelo menos cinco caracteres'
+            },
+            {
+                nome: 'idade',
+                valido: idadeValida,
+                mensagem: 'O cliente precisa ser maior do que 18 anos.'
+            }
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if(existemErros) {
+            res.status(400).json(erros)
+        } else {
+
+            const clienteValidado = {...valores, data_nascimento, data_criacao}
+
+            const sql = `UPDATE cliente SET ? WHERE id_cliente = ${id}`
+
+            conexao.query(sql, clienteValidado, (erro) => {
+                if(erro) {
+                    res.status(400).json(erro)
+                } else {
+                    res.status(200).json({...valores, id})
+                }
+            })
+
+        }
+    }
+
+    deletaCliente(id, res) {
+
+        const sql = 'DELETE FROM cliente WHERE id_cliente = ?'
+
+        conexao.query(sql, id, (erro) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({id})
+            }
+        })
+    }
+
+    calculaIdade(clientes) {
+        const anoDeNascimento = moment(clientes.data_nascimento, 'DD/MM/YYYY').format('YYYY')
+        const anoAtual = moment().format('YYYY')
+        const idadeCliente = anoAtual - anoDeNascimento
+
+        return idadeCliente
     }
 }
 
