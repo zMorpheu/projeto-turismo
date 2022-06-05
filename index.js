561a1df182df110d10551995ddf3fa9315b080d8
Lorena Santana <lo.santana96@gmail.com>
2022-06-05 01:58:24 -0300

    Ajuste na porta do servidor

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ conexao.connect(erro => {
         
         app.set('views', './views');
         app.set('view engine', 'ejs');
-        app.listen(3001, () => console.log('servidor rodando na porta 3000'))
+        app.listen(3000, () => console.log('servidor rodando na porta 3000'))
         
     }
 })
